1c09cba0b8f177260efe0147df0f35bade6b86d3
Omar Laurino <olaurino@cfa.harvard.edu>
2015-07-27 10:36:57 -0400

    add recipes for xspec depenencies

---
+++ recipes/CCfits/build.sh
@@ -0,0 +1,11 @@
+CCFITS_VER=2.4
+
+wget -N http://heasarc.gsfc.nasa.gov/fitsio/ccfits/CCfits-${CCFITS_VER}.tar.gz 
+tar xf CCfits-${CCFITS_VER}.tar.gz;
+
+cd CCfits
+
+./configure --prefix=$PREFIX
+make
+make install
+
---
+++ recipes/cfitsio/build.sh
@@ -0,0 +1,11 @@
+CFITSIO_VER=3370
+
+wget -N ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio${CFITSIO_VER}.tar.gz 
+tar xf cfistio${CFITSIO_VER}.tar.gz;
+
+cd cfitsio
+
+./configure --prefix=$PREFIX
+make
+make install
+
